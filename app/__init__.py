77d485d55364a9a12f1022b42ef1eb1d1de516cf
Ivana <ivanacramos@gmail.com>
2021-05-02 11:53:12 -0700

    "initial commit to Planet

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app(test_config=None):
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,3 @@
 from flask import Blueprint
 
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
