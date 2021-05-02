92126da433c52ebfd235014e324c58dbb779d57f
Stella Shi <shiyu.stella.shi@gmail.com>
2021-05-02 16:00:37 -0400

    "Set up migration and create the POST funct"

--- app/__init__.py
+++ app/__init__.py
@@ -9,4 +9,14 @@ migrate = Migrate()
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    from app.models.planet import Planet
+
+    from .routes import planets_bp
+    app.register_blueprint(planets_bp)
+
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,21 @@
 from flask import Blueprint
+from app import db 
+from app.models.planet import planet 
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods = ["POST","GET"])
+def handle_planets():
+    if request.method == "POST":
+        request_body = request.get_json()
+        new_planet = Planet(name = request_body["name"],
+            description = request_body["description"],
+            order_from_sun = request_body["order_from_sun"]) 
+        
+        db.session.add(new_planet) 
+        db.session.commit()
+        
+        return make_response(f"Planet {new_planet.name} successfully created", 201)
+
+    #elif request.method == "GET":
+         
